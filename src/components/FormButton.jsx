bb5fcc12e6a80995b15b66e499a274702899d145
Marc Altmann <mail@marcaltmann.com>
2016-08-31 10:03:54 +0200

    Add optional confirmation message to form button

--- src/components/FormButton.jsx
+++ src/components/FormButton.jsx
@@ -6,11 +6,12 @@ export default class FormButton extends Component {
     action: PropTypes.string.isRequired,
     method: PropTypes.string.isRequired,
     authToken: PropTypes.string.isRequired,
+    onSubmit: PropTypes.func.isRequired,
     className: PropTypes.string
   }
 
   render() {
-    const { action, method, authToken, children, className } = this.props
+    const { action, method, authToken, children, onSubmit, className } = this.props
 
     let combinedClassName
     if (className) {
@@ -18,10 +19,12 @@ export default class FormButton extends Component {
     }
 
     return(
-      <form action={action} method='POST'>
+      <form action={action} method='POST' ref={(ref) => this._form = ref}>
         <input type='hidden' name='authenticity_token' value={authToken} />
         <input type='hidden' name='_method' value={method} />
-        <button className={combinedClassName} type='submit'>{children}</button>
+        <button className={combinedClassName} type='submit'
+          onClick={(event) => onSubmit(event, this._form)}>
+          {children}</button>
       </form>
     )
   }
--- src/containers/FormButton.js
+++ src/containers/FormButton.js
@@ -1,8 +1,19 @@
 import { connect } from 'react-redux'
 import FormButton from '../components/FormButton'
 
-const mapStateToProps = (state) => ({
+const mapStateToProps = (state, ownProps) => ({
   authToken: state.authToken,
+
+  onSubmit: function(event, form) {
+    const confirmationMessage = ownProps.confirm
+
+    if (typeof confirmationMessage === 'string') {
+      event.preventDefault()
+
+      const result = window.confirm(confirmationMessage)
+      if (result) { form.submit() }
+    }
+  }
 })
 
 const mapDispatchToProps = () => ({
